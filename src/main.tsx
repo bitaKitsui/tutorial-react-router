85e2a9de0dc7c87db6454f5df202a957e3ea9334
bitaKitsui <takaki_kitsui@bita.jp>
2023-08-24 11:15:57 +0900

    feat: handling not found errors

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { Root } from "./routes/root.tsx";
+import { ErrorPage } from "./ErrorPage.tsx";
 
-const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Root />, errorElement: <ErrorPage /> },
+]);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
