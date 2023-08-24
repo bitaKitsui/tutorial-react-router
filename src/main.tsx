e3426a6074e84e2146b24e6addb3f13d326885ed
bitaKitsui <takaki_kitsui@bita.jp>
2023-08-24 11:03:42 +0900

    feat: adding router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
+const router = createBrowserRouter([
+  { path: "/", element: <div>Hello world!</div> },
+]);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
