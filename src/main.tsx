c8133cf8a7e3f9c668adb6543b1154aa97472bab
bitaKitsui <takaki_kitsui@bita.jp>
2023-08-24 11:09:18 +0900

    feat: the root route

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import { Root } from "./routes/root.tsx";
 
-const router = createBrowserRouter([
-  { path: "/", element: <div>Hello world!</div> },
-]);
+const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
