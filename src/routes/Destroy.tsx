aa5c2734d54f4e3378030804135af165604de9eb
bitaKitsui <takaki_kitsui@bita.jp>
2023-08-25 11:49:45 +0900

    feat: contextual errors

--- src/routes/Destroy.tsx
+++ src/routes/Destroy.tsx
@@ -2,6 +2,7 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts.ts";
 
 export const action = async ({ params }) => {
+  throw new Error("oh dang!");
   await deleteContact(params.contactId);
   return redirect("/");
 };
